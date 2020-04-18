849e7ec300fea89e1bb78cd604ad615fe15a47c7
Marcelo Oliveira <marceloeduardo244@gmail.com>
2020-04-18 16:15:24 -0300

    exercicio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
+import { isUuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Erro {
+  error: string;
+}
+
+interface TransactionCreate {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +25,60 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(todos => {
+        return todos.type === 'income';
+      })
+      .reduce((total, atual) => {
+        return total + atual.value;
+      }, 0);
+
+    const outcome = this.transactions
+      .filter(todos => {
+        return todos.type === 'outcome';
+      })
+      .reduce((total, atual) => {
+        return total + atual.value;
+      }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionCreate): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const income = this.transactions
+      .filter(todos => {
+        return todos.type === 'income';
+      })
+      .reduce((total, atual) => {
+        return total + atual.value;
+      }, 0);
+    const outcome = this.transactions
+      .filter(todos => {
+        return todos.type === 'outcome';
+      })
+      .reduce((total, atual) => {
+        return total + atual.value;
+      }, 0);
+    if (type === 'outcome' && income - outcome < value) {
+      throw Error('You dont have money!');
+    } else if (isUuid(transaction.id)) {
+      this.transactions.push(transaction);
+    }
+
+    return transaction;
   }
 }
 
